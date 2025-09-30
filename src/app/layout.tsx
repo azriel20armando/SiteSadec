5af9f29d5196d533adac26b4fa1543082405bb18
azriel@armando <148628656+azriel20armando@users.noreply.github.com>
2025-09-30 16:39:17 +0100

    Change HTML language to Portuguese
    Updated the lang attribute in the RootLayout component from 'en' to 'pt' to set the document language to Portuguese.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={`${raleway.variable} antialiased`}>
         {children}
       </body>
